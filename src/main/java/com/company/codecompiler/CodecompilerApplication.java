30672ac33b229729ac5bcccd2aa3a0b3aeb091f7
samueltumewu <samueltumewu21@gmail.com>
2023-04-30 14:16:03 +1000

    enable auto configuration

--- src/main/java/com/company/codecompiler/CodecompilerApplication.java
+++ src/main/java/com/company/codecompiler/CodecompilerApplication.java
@@ -1,13 +1,18 @@
 package com.company.codecompiler;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@Configuration
+@EnableAutoConfiguration
 public class CodecompilerApplication {
 
 	public static void main(String[] args) {
